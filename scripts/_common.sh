0a54b3e2b4a1c9b7f32ed3be631b0462be1b4699
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2017-09-19 10:53:16 +0200

    review pgsql helpers

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # Source: https://github.com/YunoHost-Apps/coin_ynh/
 
+
+ynh_psql_test_if_first_run() {
+	if [ -f /etc/yunohost/pgsql ];
+	then
+		echo "Postgresql is already installed, no need to create master password"
+	else
+		pgsql=$(ynh_string_random)
+		echo "$pgsql" >> /etc/yunohost/pgsql
+		psql postgres -c "\\password postgres" < echo "$pgsql"
+		systemctl service enable postgres
+		systemctl service start postgres
+	fi
+}
+
 # Open a connection as a user
 #
 # example: ynh_psql_connect_as 'user' 'pass' <<< "UPDATE ...;"
@@ -11,7 +25,10 @@
 # | arg: pwd - the user password
 # | arg: db - the database to connect to
 ynh_psql_connect_as() {
-	ynh_die "ynh_psql_connect_as is not yet implemented"
+	user="$1"
+	pwd="$2"
+	db="$3"
+	psql "${db}" --username="${user}" --password < echo "${pwd}"
 }
 
 # # Execute a command as root user
@@ -22,7 +39,7 @@ ynh_psql_connect_as() {
 ynh_psql_execute_as_root () {
 	sql="$1"
 	db="$2"
-	psql "${db}" <<< "${sql}"
+	psql "${db}" --username="postgres" <<< "${sql}"
 }
 
 # Execute a command from a file as root user
@@ -31,7 +48,29 @@ ynh_psql_execute_as_root () {
 # | arg: file - the file containing SQL commands
 # | arg: db - the database to connect to
 ynh_psql_execute_file_as_root() {
-	ynh_die "ynh_psql_execute_file_as_root is not yet implemented"
+	file="$1"
+	db="$2"
+	ynh_psql_connect_as "postgres" "$db" "$(cat /etc/yunohost/pgsql)" <<< "$file"
+}
+
+# Create a database, an user and its password. Then store the password in the app's config
+#
+# After executing this helper, the password of the created database will be available in $db_pwd
+# It will also be stored as "psqlpwd" into the app settings.
+#
+# usage: ynh_psql_setup_db user name [pwd]
+# | arg: user - Owner of the database
+# | arg: name - Name of the database
+# | arg: pwd - Password of the database. If not given, a password will be generated
+ynh_psql_setup_db () {
+	db_user="$1"
+	app="$1"
+	db_name="$2"
+	new_db_pwd=$(ynh_string_random)	# Generate a random password
+	# If $3 is not given, use new_db_pwd instead for db_pwd.
+	db_pwd="${3:-$new_db_pwd}"
+	ynh_psql_create_db "$db_name" "$db_user" "$db_pwd"	# Create the database
+	ynh_app_setting_set "$app" psqlpwd "$db_pwd"	# Store the password in the app's config
 }
 
 # Create a database and grant optionnaly privilegies to a user
@@ -39,9 +78,12 @@ ynh_psql_execute_file_as_root() {
 # usage: ynh_psql_create_db db [user [pwd]]
 # | arg: db - the database name to create
 # | arg: user - the user to grant privilegies
+# | arg: pwd  - the user password
 ynh_psql_create_db() {
 	db="$1"
 	user="$2"
+	pwd="$3"
+	ynh_psql_create_user "$user" "$pwd"
 	createdb --owner="${user}" "${db}"
 }
 
@@ -62,7 +104,8 @@ ynh_psql_drop_db() {
 # | arg: db - the database name to dump
 # | ret: the psqldump output
 ynh_psql_dump_db() {
-	ynh_die "ynh_psql_dump_db is not yet implemented"
+	db="$1"
+	pg_dump "${db}"
 }
 
 
