9da1fa4979079e4052175f1fd52d518ecd2c5759
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2017-10-06 10:37:19 +0200

    Correct typo in common, add armhf for raspberry, and comply with YEP-1.3 & YEP-2.4

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,7 +10,7 @@ ynh_psql_test_if_first_run() {
 		systemctl start postgresql
                 su --command="psql -c\"ALTER user postgres WITH PASSWORD '${pgsql}'\"" postgres
 		# we can't use peer since YunoHost create users with nologin
-		sed -i '/local\s*all\s*all\s*peer/i
+		sed -i '/local\s*all\s*all\s*peer/i \
 		local all all password' /etc/postgresql/9.4/main/pg_hba.conf
 		systemctl enable postgresql
 		systemctl reload postgresql
