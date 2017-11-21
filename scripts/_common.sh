fce38ffb8de719fe7c49b69d5f8243e3b3fb9afe
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2017-11-21 13:09:12 +0100

    fully use socket activation

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,26 +3,25 @@
 current_version="2.17.1"
 
 ynh_check_global_uwsgi_config () {
+	uwsgi --version || ynh_die "You need to add uwsgi (and appropriate plugin) as a dependency"
+
 	if [ -f /etc/systemd/system/uwsgi-app@.service ];
 	then
 		echo "Uwsgi generic file is already installed"
 	else
 		cp ../conf/uwsgi-app@.socket /etc/systemd/system/uwsgi-app@.socket
 		cp ../conf/uwsgi-app@.service /etc/systemd/system/uwsgi-app@.service
-
-		# create uwsgi group
-		groupadd uwsgi
-
-		# make sure the folder for sockets exists and set authorizations
-		mkdir -p /var/run/uwsgi/
-		chown root:uwsgi /var/run/uwsgi/
-		chmod -R 775 /var/run/uwsgi/
-
-		# make sure the folder for logs exists and set authorizations
-		mkdir -p /var/log/uwsgi/app/
-		chown root:uwsgi /var/log/uwsgi/app/
-		chmod -R 775 /var/log/uwsgi/app/
 	fi
+
+	# make sure the folder for sockets exists and set authorizations
+	mkdir -p /var/run/uwsgi/
+	chown root:www-data /var/run/uwsgi/
+	chmod -R 775 /var/run/uwsgi/
+
+	# make sure the folder for logs exists and set authorizations
+	mkdir -p /var/log/uwsgi/app/
+	chown root:www-data /var/log/uwsgi/app/
+	chmod -R 775 /var/log/uwsgi/app/
 }
 
 # Create a dedicated uwsgi ini file to use with generic uwsgi service
@@ -37,14 +36,15 @@ ynh_check_global_uwsgi_config () {
 #   __FINALPATH__ by  $final_path
 #
 # usage: ynh_add_systemd_config
-ynh_add_uwsgi_config () {
+ynh_add_uwsgi_service () {
 	ynh_check_global_uwsgi_config
 
-	usermod --append --groups uwsgi "$app" || ynh_die "It wasn't possible to add user $app to group uwsgi"
+	# www-data group is needed since it is this nginx who will start the service
+	usermod --append --groups www-data "$app" || ynh_die "It wasn't possible to add user $app to group www-data"
 
 	finaluwsgiini="/etc/uwsgi/apps-available/$app.ini"
 	ynh_backup_if_checksum_is_different "$finaluwsgiini"
-	sudo cp ../conf/uwsgi.ini "$finaluwsgiini"
+	cp ../conf/uwsgi.ini "$finaluwsgiini"
 
 	# To avoid a break by set -u, use a void substitution ${var:-}. If the variable is not set, it's simply set with an empty variable.
 	# Substitute in a nginx config file only if the variable is not empty
@@ -57,22 +57,23 @@ ynh_add_uwsgi_config () {
 	ynh_store_file_checksum "$finaluwsgiini"
 
 	chown root: "$finaluwsgiini"
-	systemctl enable "uwsgi-app@$app.service"
+	systemctl enable "uwsgi-app@$app.socket"
+	systemctl start "uwsgi-app@$app.socket"
 	systemctl daemon-reload
 
 	# Add as a service
-	yunohost service add "uwsgi-app@$app.service" --log "/var/run/uwsgi/$app.socket"
+	yunohost service add "uwsgi-app@$app.socket" --log "/var/log/uwsgi/app/$app"
 }
 
 # Remove the dedicated uwsgi ini file
 #
 # usage: ynh_remove_systemd_config
-ynh_remove_uwsgi_config () {
+ynh_remove_uwsgi_service () {
 	finaluwsgiini="/etc/uwsgi/apps-available/$app.ini"
 	if [ -e "$finaluwsgiini" ]; then
-		systemctl stop "uwsgi-app@$app.service"
-		systemctl disable "uwsgi-app@$app.service"
-		yunohost service remove "uwsgi-app@$app.service"
+		systemctl stop "uwsgi-app@$app.socket"
+		systemctl disable "uwsgi-app@$app.socket"
+		yunohost service remove "uwsgi-app@$app.socket"
 
 		ynh_secure_remove "$finaluwsgiini"
 	fi
@@ -107,7 +108,7 @@ ynh_check_if_checksum_is_different() {
 	local checksum_value=$(ynh_app_setting_get $app $checksum_setting_name)
 	local check=0
 
-	if ! echo "$checksum_value $file" | sudo md5sum -c --status
+	if ! echo "$checksum_value $file" | md5sum -c --status
 	then	# If the checksum is now different
 		check=1
 	fi
