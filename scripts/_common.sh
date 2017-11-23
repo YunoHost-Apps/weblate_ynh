c0db10fa59612aeebb40e795ebc5c0646a05e8b5
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2017-11-23 14:02:56 +0100

    Add postgres 9.6 support for stretch

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -122,12 +122,24 @@ ynh_psql_test_if_first_run() {
 		echo "PostgreSQL is already installed, no need to create master password"
 	else
 		pgsql=$(ynh_string_random)
+		pg_hba=""
 		echo "$pgsql" >> /etc/yunohost/psql
+
+		if [ -e /etc/postgresql/9.4/ ]
+		then
+			pg_hba=/etc/postgresql/9.4/main/pg_hba.conf
+		elif [ -e /etc/postgresql/9.6/ ]
+		then
+			pg_hba=/etc/postgresql/9.6/main/pg_hba.conf
+		else
+			ynh_die "postgresql shoud be 9.4 or 9.6"
+		fi
+
 		systemctl start postgresql
                 su --command="psql -c\"ALTER user postgres WITH PASSWORD '${pgsql}'\"" postgres
 		# we can't use peer since YunoHost create users with nologin
 		sed -i '/local\s*all\s*all\s*peer/i \
-		local all all password' /etc/postgresql/9.4/main/pg_hba.conf
+		local all all password' "$pg_hba"
 		systemctl enable postgresql
 		systemctl reload postgresql
 	fi
