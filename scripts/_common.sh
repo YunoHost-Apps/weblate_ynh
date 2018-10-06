6647a93af3ccdd9abdfddcb20b9260bfe36c3789
Jean-Baptiste Holcroft <jean-baptiste@holcroft.fr>
2018-10-06 20:50:23 +0200

    Weblate 3.2 initialization

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-current_version="3.1.1"
+current_version="3.2.0"
 
 ynh_check_global_uwsgi_config () {
 	uwsgi --version || ynh_die "You need to add uwsgi (and appropriate plugin) as a dependency"
