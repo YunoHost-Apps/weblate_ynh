b0721359ba7e444ec46ee6424e68b328d9172616
Thomas <51749973+Thovi98@users.noreply.github.com>
2024-01-17 19:12:51 +0100

    try install/remove

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,17 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? weblate_dependencies="libxml2-dev libxslt-dev libfreetype6-dev libjpeg-dev libz-dev libyaml-dev \
-   libffi-dev libcairo-dev gir1.2-pango-1.0 libgirepository1.0-dev \
-   libacl1-dev libssl-dev libpq-dev libjpeg62-turbo-dev build-essential \
-   python3-gdbm python3-dev python3-pip python3-virtualenv virtualenv git \
-   uwsgi uwsgi-plugin-python3 redis-server postgresql postgresql-contrib hub"
-
-# because weblate install borgbackup
-#REMOVEME? borgbackup_dependencies="libacl1-dev libacl1 libssl-dev liblz4-dev libzstd-dev libxxhash-dev \
-	build-essential pkg-config python3-pkgconfig"
-
 #REMOVEME? pkg_dependencies="$weblate_dependencies $borgbackup_dependencies"
 
 debian_maj_version=$(sed 's/\..*//' /etc/debian_version)
@@ -48,67 +37,6 @@ set_forge_variables() {
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
-
-# Send an email to inform the administrator
-#
-# usage: ynh_send_readme_to_admin app_message [recipients]
-# | arg: app_message - The message to send to the administrator.
-# | arg: recipients - The recipients of this email. Use spaces to separate multiples recipients. - default: root
-#	example: "root admin@domain"
-#	If you give the name of a YunoHost user, ynh_send_readme_to_admin will find its email adress for you
-#	example: "root admin@domain user1 user2"
-ynh_send_readme_to_admin() {
-	local app_message="${1:-...No specific information...}"
-	local recipients="${2:-root}"
-
-	# Retrieve the email of users
-	find_mails () {
-		local list_mails="$1"
-		local mail
-		local recipients=" "
-		# Read each mail in argument
-		for mail in $list_mails
-		do
-			# Keep root or a real email address as it is
-			if [ "$mail" = "root" ] || echo "$mail" | grep --quiet "@"
-			then
-				recipients="$recipients $mail"
-			else
-				# But replace an user name without a domain after by its email
-				if mail=$(ynh_user_get_info "$mail" "mail" 2> /dev/null)
-				then
-					recipients="$recipients $mail"
-				fi
-			fi
-		done
-		echo "$recipients"
-	}
-	recipients=$(find_mails "$recipients")
-
-	local mail_subject="☁️🆈🅽🅷☁️: \`$app\` was just installed!"
-
-	local mail_message="This is an automated message from your beloved YunoHost server.
-
-Specific information for the application $app.
-
-$app_message
-
----
-Automatic diagnosis data from YunoHost
-
-$(yunohost tools diagnosis | grep -B 100 "services:" | sed '/services:/d')"
-
-	# Define binary to use for mail command
-	if [ -e /usr/bin/bsd-mailx ]
-	then
-		local mail_bin=/usr/bin/bsd-mailx
-	else
-		local mail_bin=/usr/bin/mail.mailutils
-	fi
-
-	# Send the email to the recipients
-	echo "$mail_message" | $mail_bin -a "Content-Type: text/plain; charset=UTF-8" -s "$mail_subject" "$recipients"
-}
 
 #=================================================
 #
